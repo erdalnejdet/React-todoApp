82cbe3b50f598aadc9cbeef111ef373fcbaae01e
Nejdet erdal <erdalnejdet1@gmail.com>
2023-07-04 14:47:44 +0300

    Added-swal-modal

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './TodoApp.scss';
+import Swal from 'sweetalert2';
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
@@ -13,43 +14,51 @@ const TodoApp = () => {
     }
   }, []);
 
-  useEffect(() => {
-    // Store todos in local storage whenever it changes
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
   const handleInputChange = (event) => {
     setNewTodo(event.target.value);
   };
 
   const handleAddTodo = () => {
     if (newTodo.trim() !== '') {
-      setTodos([...todos, newTodo]);
-      setNewTodo('');
-      alert('Successfully added todo');
+      if (todos.includes(newTodo)) {
+        Swal.fire('Error!', 'Todo already exists.', 'error');
+      } else {
+        const updatedTodos = [...todos, newTodo];
+        setTodos(updatedTodos);
+        localStorage.setItem('todos', JSON.stringify(updatedTodos));
+        setNewTodo('');
+        Swal.fire('Success!', 'Todo has been added.', 'success');
+      }
     } else {
-      alert('Cannot add empty todo');
+      Swal.fire('Error!', 'Cannot add empty todo.', 'error');
     }
   };
 
   const handleDeleteTodo = (index) => {
-    const confirmDelete = window.confirm('Are you sure you want to delete this todo?');
-    if (confirmDelete) {
-      const updatedTodos = todos.filter((_, i) => i !== index);
-      setTodos(updatedTodos);
-    }
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const updatedTodos = todos.filter((_, i) => i !== index);
+        setTodos(updatedTodos);
+        localStorage.setItem('todos', JSON.stringify(updatedTodos));
+        Swal.fire('Deleted!', 'Your todo has been deleted.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your todo is safe :)', 'error');
+      }
+    });
   };
 
   return (
     <div className="todo-app">
       <h1>To-Do App</h1>
       <div className="todo-input">
-        <input
-          type="text"
-          placeholder="Add a new todo"
-          value={newTodo}
-          onChange={handleInputChange}
-        />
+        <input type="text" placeholder="Add a new todo" value={newTodo} onChange={handleInputChange} />
         <button onClick={handleAddTodo}>Add</button>
       </div>
       <ul className="todo-list">
